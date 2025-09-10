7c6c775a8a722caee192433ac549c51553375825
Hongchul Shin <mschongchulshin@gmail.com>
2025-09-10 10:53:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
 
-# requirements.txt 파일 읽기
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
     name='cad_generator',
-    version='0.1.0',
-    author='mschongchulshin',  # GitHub 사용자 이름으로 변경
-    author_email='saekomi5@korea.ac.kr',  # 제공해주신 이메일
+    version='0.1.1',
+    author='mschongchulshin',
+    author_email='saekomi5@korea.ac.kr',
     description='A fine-tuned LLM for generating CAD data from natural language.',
-    long_description=open('README.md', encoding='utf-8').read(),
+    long_description="See the GitHub repository for full details: https://github.com/mschongchulshin/finetuningcadtest20250909",
     long_description_content_type='text/markdown',
     url='https://github.com/mschongchulshin/finetuningcadtest20250909',
     packages=find_packages(),
-    install_requires=required,
+    install_requires=[
+        'transformers',
+        'torch',
+        'accelerate',
+        'huggingface_hub'
+    ],
     include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
